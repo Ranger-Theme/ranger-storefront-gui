b6a73ae7dea56ab12d59dbe7de41adfc9dbfbbd3
caesar <caesar.luo@silksoftware.com>
2024-04-02 15:25:16 +0800

    chore: remove inrequire

--- src/index.ts
+++ src/index.ts
@@ -13,52 +13,72 @@ const readPkg = (): PkgType => {
 
 const bootstrap = async () => {
   const pkg: PkgType = await readPkg()
-  // const pkgName: string = pkg.name as string
+  const pkgName: string = pkg.name as string
   const pkgVersion: string = pkg?.version ?? '0.1.0'
   // const pkgDescription: string = pkg?.description ?? ''
-  
+
   const arg = hideBin(process.argv)
   const cli = yargs(arg)
 
   cli
     .version(pkgVersion)
     .usage('Usage: $0 <command> [options]')
-    // .demandCommand(1, 'A command is required. Pass --help to see all available commands and options.')
     .recommendCommands()
     .strict()
     .alias('h', 'help')
     .alias('v', 'version')
-    .wrap(cli.terminalWidth()).epilogue(dedent`
+    .wrap(cli.terminalWidth())
+    .epilogue(
+      dedent`
       When a command fails, all logs are written to lerna-debug.log in the current working directory.
       For more information, check out the docs at https://lerna.js.org/docs/introduction
-    `)
-    .options({
-      debug: {
-        type: 'boolean',
-        alias: 'd',
-        describe: 'Boostrap debug mode'
-      }
+    `
+    )
+    .option('debug', {
+      type: 'boolean',
+      alias: 'd',
+      describe: 'Boostrap debug mode'
     })
-    .group(['debug'], 'Dev Options')
+    .option('registry', {
+      type: 'string',
+      alias: 'r',
+      describe: 'Define global registry'
+    })
+    .group(['debug'], 'Dev Options:')
+    .group(['registry'], 'Extra Options:')
     .fail((msg, err: any) => {
       // certain yargs validations throw strings :P
-      const actual = err || new Error(msg);
+      const actual = err || new Error(msg)
 
       // ValidationErrors are already logged, as are package errors
-      if (actual.name !== "ValidationError" && !actual.pkg) {
+      if (actual.name !== 'ValidationError' && !actual.pkg) {
         // the recommendCommands() message is too terse
         if (/Did you mean/.test(actual.message)) {
           // @ts-ignore
-          console.error("lerna", `Unknown command "${cli.parsed.argv._[0]}"`);
+          console.error(`${pkgName}:`, `Unknown command "${cli.parsed.argv._[0]}"`)
         }
 
-        console.error("lerna", actual.message);
+        console.error(`${pkgName}:`, actual.message)
       }
 
-      // exit non-zero so the CLI can be usefully chained
-      cli.exit(actual.exitCode > 0 ? actual.exitCode : 1, actual);
+      cli.exit(actual.exitCode > 0 ? actual.exitCode : 1, actual)
     })
-   
+
+  cli.command(
+    'create [name]',
+    'create react app',
+    (program: any) => {
+      program.option('name', {
+        type: 'sring',
+        describe: 'Project name',
+        alias: 'n'
+      })
+    },
+    (argv) => {
+      console.info(argv)
+    }
+  )
+
   cli.command({
     command: 'greet',
     describe: '向用户发送问候',
@@ -70,12 +90,13 @@ const bootstrap = async () => {
       }
     },
     handler(argv) {
-      console.log(`你好，${argv.name}！`);
+      console.log(`你好，${argv.name}！`)
     }
   })
 
   cli.argv
   cli.parse()
+  cli.showHelp()
 }
 
 bootstrap()
